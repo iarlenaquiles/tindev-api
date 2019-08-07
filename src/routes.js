a847d69bf35c8300258ac61575d85184d6b4c82a
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-08-06 21:52:49 -0300

    criando o crud para salvar, dar like, dislikes nos devs

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,12 @@
 const express = require('express');
+const DevController = require('./controllers/DevController');
+const LikeController = require('./controllers/LikeController');
+const DislikeController = require('./controllers/DislikeController');
 
 const routes = express.Router();
+routes.get('/devs', DevController.index)
+routes.post('/devs', DevController.store);
+routes.post('/devs/:devId/likes', LikeController.store);
+routes.post('/devs/:devId/dislikes', DislikeController.store);
 
-routes.get('/', (req, res) => {
-
-    return res.send('Hello World')
-})
-
-
-routes.post('/devs', (req, res) => {
-    return res.json(req.body);
-});
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
 const routes = require('./routes');
 
 const server = express();
-server.use(express.json())
+mongoose.connect('mongodb+srv://iarlen:@cluster0-sv21e.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
+server.use(cors());
+server.use(express.json());
 server.use(routes);
 
 server.listen(3333)
